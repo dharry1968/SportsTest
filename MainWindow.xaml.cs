d56e43bd3e4b7c888d1d4dd0d79a8dd1619a46b8
Dark Galaxies <dean@callit.com.au>
2023-01-25 12:05:24 +1100

    Updates

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Data;
+
 
 namespace SportsTest
 {
@@ -20,9 +25,67 @@ namespace SportsTest
     /// </summary>
     public partial class MainWindow : Window
     {
+        DataTable dt;
         public MainWindow()
         {
             InitializeComponent();
+            lstPlayers.SelectionChanged += new SelectionChangedEventHandler(lstPlayers_SelectedIndexChanged);
+            dt = new DataTable();
+            dt.Columns.Add("FirstName", typeof(string));
+            dt.Columns.Add("Surname", typeof(string));
+            dt.Columns.Add("Age", typeof(string));
+            dt.Columns.Add("Height", typeof(string));
+            dt.Columns.Add("DOB", typeof(string));
+            dt.Columns.Add("Country", typeof(string));
+            dt.Columns.Add("Continent", typeof(string));
+            this.DataContext = dt;
+        }
+
+        private async void btngetPlayers_Click(object sender, RoutedEventArgs e)
+        {
+            string apiKey = "1d4ee500-9bcb-11ed-b1b1-4ff2250657fc";
+            var response = await GetPlayers(apiKey);
+            if (response != null)
+            {
+                RootObject data = JsonConvert.DeserializeObject<RootObject>(response);
+                foreach (var player in data.data)
+                {
+                    dt.Rows.Add(player.firstname, player.lastname, player.age, player.height, player.birthday, player.country.name, player.country.continent);
+                }
+            }
+            else
+            {
+                txtStatus.Text = "No Data Recieved";
+            }
+        }
+
+        static async Task<string> GetPlayers(string apikey)
+        {
+            using (var client = new HttpClient())
+            {
+                var url = "https://app.sportdataapi.com/api/v1/soccer/players?apikey=" + apikey + "&country_id=42";
+                var resp = await client.GetAsync(url);
+                if (resp.IsSuccessStatusCode)
+                {
+                    return await resp.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        private void lstPlayers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataRowView selectedRow = (DataRowView)lstPlayers.SelectedItem;
+
+            if (selectedRow != null)
+            {
+                string name = (string)selectedRow["FirstName"] + " " + (string)selectedRow["Surname"];
+                txtPlayerName.Text = name;
+            }
         }
     }
 }
+
---
+++ Player.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SportsTest
+{
+    [Serializable]
+    internal class Player
+    {
+        [JsonProperty]
+        public int player_id;
+        [JsonProperty]
+        public string firstname;
+        [JsonProperty]
+        public string lastname;
+        [JsonProperty]
+        public string birthday;
+        [JsonProperty]
+        public int? age;
+        [JsonProperty]
+        public int? weight;
+        [JsonProperty]
+        public int? height;
+        [JsonProperty]
+        public string img;
+        [JsonProperty]
+        public Country country;
+    }
+
+    [Serializable]
+    class Country
+    {
+        [JsonProperty]
+        public int country_id;        
+        [JsonProperty]
+        public string name;
+        [JsonProperty]
+        public string country_code;
+        [JsonProperty]
+        public string continent;
+    }
+
+    [Serializable]
+    class Data
+    {
+        public List<Player> data;
+    }
+
+    [Serializable]
+    class Query
+    {
+        [JsonProperty]
+        public int country_id;
+        [JsonProperty]
+        public int max_age;
+        [JsonProperty]
+        public string apikey;
+    }
+
+    [Serializable]
+    class RootObject
+    {
+        [JsonProperty]
+        public Query query;
+        [JsonProperty]
+        public List<Player> data;
+    }
+}
